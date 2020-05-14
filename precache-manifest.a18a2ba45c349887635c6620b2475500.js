a68522847f4cf6827f94037c3d775ccdb279bcdc
Oscar Nunez <noscar07@gmail.com>
2020-05-13 22:34:21 -0400

    Updates

---
+++ precache-manifest.a18a2ba45c349887635c6620b2475500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e40e34a2b6f07c3c9deec9af2a9411",
+    "url": "/%7BemployeeDir%7D/index.html"
+  },
+  {
+    "revision": "73c6c01ab51f55039b24",
+    "url": "/%7BemployeeDir%7D/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b216cc4b4a4203f493c2",
+    "url": "/%7BemployeeDir%7D/static/js/2.1322e01d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BemployeeDir%7D/static/js/2.1322e01d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c6c01ab51f55039b24",
+    "url": "/%7BemployeeDir%7D/static/js/main.0d2db843.chunk.js"
+  },
+  {
+    "revision": "bb431f2b479b636d7294",
+    "url": "/%7BemployeeDir%7D/static/js/runtime-main.3bde9412.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%7BemployeeDir%7D/static/media/logo.5d5d9eef.svg"
+  }
+]);
